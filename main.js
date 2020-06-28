09d77d4958e8c557126caa71c7460d32e3b30a74
Amit Verma <amitverma.ukind@gmail.com>
2020-06-28 12:42:22 +0530

    Graph Drawn

--- main.js
+++ main.js
@@ -12,3 +12,21 @@ for (i = 50; i < 500; i += 50) {
     ctx.lineTo(i, canvas.width);
     ctx.stroke();
 }
+
+ctx.beginPath();
+ctx.moveTo(0, 0);
+ctx.lineTo(50, 0);
+ctx.lineTo(50, 450);
+ctx.lineTo(500, 450);
+ctx.lineTo(500, 500);
+ctx.lineTo(0, 500);
+ctx.fillStyle = "#E5E4E2";
+ctx.fill();
+ctx.fillStyle = "#000000";
+ctx.font = "bold 20px Arial";
+
+var counter = 4;
+for (i = 50; i < 500; i += 50) {
+    ctx.fillText(counter, 25, i + 5);
+    ctx.fillText(-counter--, i - 5, 475);
+}
