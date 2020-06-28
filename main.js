85d57e7264492cc286e7ccc0f8522ae7214903f3
Amit Verma <amitverma.ukind@gmail.com>
2020-06-28 12:12:26 +0530

    Drawing Lines

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Working");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+// Drawing Graph Lines
+for (i = 50; i < 500; i += 50) {
+  ctx.moveTo(0, i);
+  ctx.lineTo(canvas.width, i);
+  ctx.stroke();
+  ctx.moveTo(i, 0);
+  ctx.lineTo(i, canvas.width);
+  ctx.stroke();
+}
