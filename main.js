e669dbcd856b4c1b9bdca1fb947bf937a0dcb1a1
Amit Verma <amitverma.ukind@gmail.com>
2020-06-28 12:25:56 +0530

    Drawing Axes

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ var ctx = canvas.getContext("2d");
 
 // Drawing Graph Lines
 for (i = 50; i < 500; i += 50) {
-  ctx.moveTo(0, i);
-  ctx.lineTo(canvas.width, i);
-  ctx.stroke();
-  ctx.moveTo(i, 0);
-  ctx.lineTo(i, canvas.width);
-  ctx.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = i === 250 ? 3 : 1;
+    ctx.moveTo(0, i);
+    ctx.lineTo(canvas.width, i);
+    ctx.stroke();
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, canvas.width);
+    ctx.stroke();
 }
